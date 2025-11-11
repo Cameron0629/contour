70478276e839bdb17b501c8e6db2a2a5e803ff31
Cameron0629 <cameronwright.0629@gmail.com>
2025-11-10 20:52:34 -0500

    Add ios contour and web contour

---
+++ iosContour/Contour/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Contour
+//
+//  Created by Cameron Wright on 11/8/25.
+//
+
+import SwiftUI
+
+
+struct ContentView: View{
+    var body: some View{
+        Image("shirt")
+    }
+}
+#Preview {
+    ContentView()
+}
